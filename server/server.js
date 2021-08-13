319eace1af6c3dcef95a2fb45c35c9b5bf8f7b06
daniml3 <danimoral1001@gmail.com>
2021-08-14 01:27:52 +0200

    {client, server}: add support for password-based security
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I0c48005d1abd69239a50f44bd91aae53006a5397

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var multiparty = require("multiparty");
 var fs = require("fs");
+var credentialManager = require("./auth/credentialmanager.js");
 
 var app = express();
+var insecure = (process.env.CLOUDDRIVE_INSECURE == 1);
 
 global.port = process.env.CLOUDDRIVE_PORT || 3000;
 global.fileStorage = process.env.CLOUDDRIVE_STORAGE ||  __dirname + "/" + "./files/";
@@ -25,12 +27,29 @@ app.listen(global.port, () => {
     global.LOG(INFO, "Temporal storage directory at " + global.tempFileStorage);
 });
 
-app.use(express.static("../client/"));
+app.use(express.static("../client/", {extensions: ["html"]}));
 
 try {
     fs.rmdirSync(global.tempFileStorage, {recursive: true});
     global.LOG(global.INFO, "Cleaned the temporal files directory");
 } catch (err) {}
+
+var authConfigFile = process.env.CLOUDDRIVE_AUTH_CONFIG || __dirname + "/" + "../authConfig.json";
+try {
+    credentialManager.parseConfig(JSON.parse(fs.readFileSync(authConfigFile, "utf8")));
+} catch (err) {
+    global.LOG(global.ERROR, "Failed to load the auth config " + authConfigFile);
+    global.LOG(global.ERROR, err);
+    if (insecure) {
+        global.LOG(global.WARNING, "########## SECURITY ERROR ##########");
+        global.LOG(global.WARNING, "Running in insecure mode as told to");
+        global.LOG(global.WARNING, "If this isn't intentional, fix the errors of the auth config and restart the server");
+        global.LOG(global.WARNING, "####################################");
+        credentialManager.setInsecure(true);
+    } else {
+        process.exit(1);
+    }
+}
 
 var routeList = fs.readdirSync("./routes/");
 for (i = 0; i < routeList.length; i++) {
