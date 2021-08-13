27657103085af8f4633df05b747d4360a8881066
daniml3 <danimoral1001@gmail.com>
2021-08-14 01:27:52 +0200

    {client, server}: add support for password-based security
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I0c48005d1abd69239a50f44bd91aae53006a5397

---
+++ server/middleware/tokenverifier.js
@@ -0,0 +1,47 @@
+var multiparty = require("multiparty");
+var credentialManager = require('../auth/credentialmanager');
+
+const neededFormKeys = ["sessionToken"];
+
+function verifyToken(req, res, next) {
+    var token = req.query.sessionToken;
+    var isTokenValid;
+    var response = {};
+
+    if (token) {
+        isTokenValid = credentialManager.isTokenValid(token);
+        if (isTokenValid) {
+            next();
+            return;
+        }
+    }
+
+    response["error"] = true;
+    response["denied"] = true;
+    response["errorMessage"] = "Invalid credentials";
+    res.send(response);
+};
+
+function verifyOneTimeToken(req, res, next) {
+    var token = req.query.sessionToken;
+    var isTokenValid;
+    var response = {};
+
+    if (token) {
+        isTokenValid = credentialManager.isTokenValidOnce(token);
+        if (isTokenValid) {
+            next();
+            return;
+        }
+    }
+
+    response["error"] = true;
+    response["denied"] = true;
+    response["errorMessage"] = "Invalid credentials";
+    res.send(response);
+};
+
+module.exports = {
+    verifyToken: verifyToken,
+    verifyOneTimeToken: verifyOneTimeToken
+};
