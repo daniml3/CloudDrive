6686ce65d751363207a2cf7dc0113dfc8a729f7f
daniml3 <danimoral1001@gmail.com>
2021-08-14 01:27:52 +0200

    {client, server}: add support for password-based security
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I0c48005d1abd69239a50f44bd91aae53006a5397

---
+++ client/assets/js/login.js
@@ -0,0 +1,32 @@
+document.sessionHandler = new SessionHandler();
+document.authManager = new AuthManager();
+document.cookieHandler = new CookieHandler();
+
+var sessionHandler = document.sessionHandler;
+var authManager = document.authManager;
+var cookieHandler = document.cookieHandler;
+
+window.onload = function() {
+    sessionHandler.init();
+    authManager.init(true);
+
+    document.getElementById("login-button").onclick = function() {
+        var username = document.getElementById("username-input").value;
+        var password = document.getElementById("password-input").value;
+        var statusText = document.getElementById("login-status-text");
+
+        if (!(username && password)) {
+            statusText.innerHTML = "Missing credentials";
+        } else {
+            statusText.innerHTML = "Verifying credentials";
+            authManager.login(username, password, function(loggedIn) {
+                if (loggedIn) {
+                    statusText.innerHTML = "Redirecting";
+                    sessionHandler.goToMain();
+                } else {
+                    statusText.innerHTML = "Invalid credentials";
+                }
+            });
+        }
+    };
+};
