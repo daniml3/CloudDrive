a6cb142b85626a48c8e2884fd98627dab4d18b2b
daniml3 <danimoral1001@gmail.com>
2021-08-22 01:46:27 +0200

    {client, server}: wait for directory changes using fs.watch
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I580a01e46080776291361eda2b0ce99908a381eb

--- server/routes/readdir.js
+++ server/routes/readdir.js
@@ -37,4 +37,34 @@ module.exports = function (app) {
             res.send(response);
         });
     });
+
+    app.post("/watchdir", tokenVerifier.verifyToken, function (req, res) {
+        formparser.parseForm(req, res, neededFormKeys, function(fields, files, generatedForm, response) {
+            var targetDirectory = generatedForm["targetDirectory"];
+
+            if (targetDirectory.includes("..")) {
+                global.LOG(global.WARNING, "Tried to perform an illegal operation "
+                           + "(tried to access to the directory " + targetDirectory + ")");
+                response["error"] = true;
+                response["errorMessage"] = "Illegal request";
+                res.send(response);
+                return;
+            }
+
+            response["error"] = false;
+            var directory = global.fileStorage + "/" + targetDirectory + "/";
+            try {
+                fs.watch(directory, function(eventType, filename) {
+                    try {
+                        res.send(response);
+                    } catch (err) {
+                        // TODO: figure out why this errors even if the request has been sent successfully
+                    }
+                });
+            } catch (err) {
+                response["error"] = true;
+                res.send(response);
+            }
+        });
+    });
 }
