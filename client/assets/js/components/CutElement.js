3b503e696dcac429a296fd4c036d2a315b73237f
daniml3 <danimoral1001@gmail.com>
2021-09-02 00:50:32 +0100

    client: implement file / directory cutting and pasting
    * It was already implemented on the server - only the client
      implementation was left :)
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I70100c3327ef80c488ae9d2102982117181f5105

---
+++ client/assets/js/components/CutElement.js
@@ -0,0 +1,27 @@
+class CutElement {
+    constructor(filename, elementPath) {
+        this.filename = filename;
+        this.elementPath = elementPath;
+    }
+
+    addToList() {
+        var element = document.createElement("div");
+        var container = document.getElementById("cut-files-container");
+        var cutElement = this;
+
+        element.classList.add("material-button");
+        element.classList.add("cut-element");
+        element.innerHTML = this.elementPath;
+        element.onclick = function() {
+            document.cutHandler.uncut(cutElement.elementPath);
+        };
+
+        container.appendChild(element);;
+        this.element = element;
+    }
+
+    removeFromList() {
+        var container = document.getElementById("cut-files-container");
+        container.removeChild(this.element);
+    }
+}
