068cd6c2bed5c86dd202cbff62235cc899b25d2c
daniml3 <danimoral1001@gmail.com>
2021-10-10 01:26:51 +0200

    server: save the logs to a file
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I93754f114999b9e2e00a86cd514ae32d2f04e8ce

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,10 +1,18 @@
+const logPath = process.env.CD_WORKING_DIRECTORY + "/last-log.txt";
+var fs = require("fs");
+
 function log(level, text) {
-    console.log(new Date().toISOString() + " [" + level + "]: " + text);
+    var content = new Date().toISOString() + " [" + level + "]: " + text;
+    console.log(content);
+    try {
+      fs.appendFileSync(logPath, content + "\n");
+    } catch (err) {}
 }
 
 module.exports = {
     INFO: "INFO",
     WARNING: "WARNING",
     ERROR: "ERROR",
+    LOG_PATH: logPath,
     LOG: log
 };
